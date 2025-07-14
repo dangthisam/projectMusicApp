9b6c01a93fb6126446d88d9e3efa415bb24a9258
Nguyễn Văn Sâm <vansam09082004@gmail.com>
2025-07-14 21:35:56 +0700

    connect database

--- server.ts
+++ server.ts
@@ -1,1 +1,18 @@
-console.log("ok")
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+import connect from"./config/connectDB";
+connect();
+const port: number |string= process.env.PORT || 3000;
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.get("/topics", (req: Request, res: Response) => {
+   res.render("client/pages/topics/index.pug");
+});
+
+app.listen(port, () => {
+   console.log(`Server running at http://localhost:${port}`);
+});
